0e9066503ee67531ec16f073d9ed61534b489e8b
Vadim Malyshev <vadim@localhost.localdomain>
2017-01-14 15:19:58 +0300

    sequence redesign for beter pemormance

---
+++ src/vds_core/async_task.cpp
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2017, Vadim Malyshev, lboss75@gmail.com
+All rights reserved
+*/
+#include "stdafx.h"
+#include "service_provider.h"
+#include "mt_service.h"
+#include "async_task.h"
+
+vds::async_task_base::~async_task_base()
+{
+}
