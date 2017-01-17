ff54225a3381326eedf021670dbd4052381c2ca9
Vadim Malyshev <v.malyshev@innopolis.ru>
2017-01-17 08:37:22 +0300

    add missing file

---
+++ src/vds_network/socket_connect.h
@@ -0,0 +1,88 @@
+﻿#ifndef __VDS_NETWORK_SOCKET_CONNECT_H_
+#define __VDS_NETWORK_SOCKET_CONNECT_H_
+
+/*
+Copyright (c) 2017, Vadim Malyshev, lboss75@gmail.com
+All rights reserved
+*/
+
+#include "network_socket.h"
+
+namespace vds {
+  class socket_connect
+  {
+  public:
+    socket_connect(const service_provider & sp)
+      : network_service_(sp.get<inetwork_manager>().owner_)
+    {
+    }
+
+    template <
+      typename done_method_type,
+      typename error_method_type
+    >
+    class handler
+    {
+    public:
+      handler(
+        done_method_type & done_method,
+        error_method_type & error_method,
+        const socket_connect & owner
+      )
+        : done_method_(done_method), error_method_(error_method),
+        s_(
+#ifdef _WIN32
+          WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED)
+#else
+          socket(PF_INET, SOCK_STREAM, 0)
+#endif//_WIN32
+        ), network_service_(owner.network_service_)
+      {
+      }
+
+      void operator()(
+        const std::string & address,
+        u_int16_t port
+      )
+      {
+        // Connexion setting for local connexion 
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(address.c_str());
+        addr.sin_port = htons(port);
+
+#ifdef _WIN32
+        // Connect 
+        if (SOCKET_ERROR == ::connect(this->s_.handle(), (struct sockaddr *)&addr, sizeof(addr))) {
+          // As we are in non-blocking mode we'll always have the error 
+          // WSAEWOULDBLOCK whichis actually not one 
+          auto error = WSAGetLastError();
+          if (WSAEWOULDBLOCK != error) {
+            throw new std::system_error(error, std::system_category(), "connect");
+          }
+        }
+
+        this->network_service_->associate(this->s_.handle());
+#else
+        // Connect 
+        if (0 > ::connect(this->s_.handle(), (struct sockaddr *)&addr, sizeof(addr))) {
+          auto error = errno;
+          throw new std::system_error(error, std::system_category());
+        }
+#endif
+        this->done_method_(this->s_);
+      }
+
+    private:
+      network_service * network_service_;
+      const done_method_type & done_method_;
+      const error_method_type & error_method_;
+      network_socket s_;
+    };
+  private:
+    network_service * network_service_;
+  };
+}
+
+
+#endif//__VDS_NETWORK_SOCKET_CONNECT_H_
