3582125431ac64f5ffa8771ff5766479b06e8c14
Vadim Malyshev <lboss75@gmail.com>
2017-09-30 17:38:05 +0300

    move files from server

--- libs/vds_server/cert_manager.cpp
+++ libs/vds_server/cert_manager.cpp
@@ -19,3 +19,18 @@ bool vds::_cert_manager::validate(
 {
   return true;
 }
+
+void vds::_cert_manager::create_database_objects(
+  const service_provider & sp,
+  uint64_t db_version,
+  database_transaction & t)
+{
+  if (1 > db_version) {
+    t.execute(
+      "CREATE TABLE cert(\
+      id VARCHAR(64) NOT NULL,\
+      body BLOB NOT NULL,\
+      parent VARCHAR(64) NOT NULL,\
+      CONSTRAINT pk_cert PRIMARY KEY(id))");
+  }
+}
--- libs/vds_server/cert_manager_p.h
+++ libs/vds_server/cert_manager_p.h
@@ -7,12 +7,36 @@ All rights reserved
 */
 
 #include "cert_manager.h"
+#include "database_orm.h"
 
 namespace vds {
   class _cert_manager : public cert_manager
   {
   public:
     bool validate(const certificate & cert);
+    
+    static void create_database_objects(
+      const service_provider & sp,
+      uint64_t db_version,
+      database_transaction & t);
+
+  private:
+    class cert_table : public database_table
+    {
+    public:
+      cert_table()
+      : database_table("cert"),
+        id(this, "id"),
+        cert(this, "body"),
+        parent(this, "parent")
+      {
+      }
+      
+      database_column<guid> id;
+      database_column<const_data_buffer> cert;
+      database_column<guid> parent;
+    };
+
   };
 }
 
