d549805852879a03805f2c8ea8a0087dfc56f8a1
Vadim Malyshev <v.malyshev@innopolis.ru>
2017-10-03 15:22:47 +0300

    derive p2p layer

---
+++ kernel/vds_peer2peer/stdafx.h
@@ -0,0 +1,15 @@
+#ifndef __VDS_PEER2PEER_STDAFX_H_
+#define __VDS_PEER2PEER_STDAFX_H_
+
+/*
+Copyright (c) 2017, Vadim Malyshev, lboss75@gmail.com
+All rights reserved
+*/
+
+#include <exception>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+
+#endif//__VDS_PEER2PEER_STDAFX_H_
---
+++ kernel/vds_peer2peer/vds_peer2peer.h
@@ -0,0 +1,13 @@
+#ifndef __VDS_PEER2PEER_PEER2PEER_H_
+#define __VDS_PEER2PEER_PEER2PEER_H_
+
+/*
+Copyright (c) 2017, Vadim Malyshev, lboss75@gmail.com
+All rights reserved
+*/
+
+namespace vds {
+
+}
+
+#endif//__VDS_PEER2PEER_PEER2PEER_H_
