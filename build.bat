15c3e66ad54da805f1584d7aaaff69d09a6fbb2b
UNI\v.malyshev <v.malyshev@innopolis.ru>
2017-12-21 17:40:37 +0300

    fix windows build

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ rmdir %root_folder%\build /s /q
 mkdir %root_folder%\build
 cd %root_folder%\build
 
-cmake.exe  -DZLIB_INCLUDE_DIR=%root_folder%\externals\zlib_out\include -DZLIB_LIBRARY=%root_folder%\externals\zlib_out\lib\zlibstaticd.lib -DOPENSSL_ROOT_DIR=%root_folder%\externals\Build-VC-64-debug\ -DGTEST_LIBRARY=%root_folder%\externals\build_gtest\Debug\gtestd.lib -DGTEST_INCLUDE_DIR=%root_folder%\externals\googletest\include -DGTEST_MAIN_LIBRARY=%root_folder%\externals\build_gtest\Debug\gtest_maind.lib ../ -G "Visual Studio 15 2017 Win64"
+cmake.exe  -DZLIB_INCLUDE_DIR=%root_folder%\externals\zlib_out\include -DZLIB_LIBRARY=%root_folder%\externals\zlib_out\lib\zlibstaticd.lib -DOPENSSL_ROOT_DIR=%root_folder%\externals\Build-VC-64-debug\ -DGTEST_LIBRARY=%root_folder%\externals\build_gtest\Debug\gtestd.lib -DGTEST_INCLUDE_DIR=%root_folder%\externals\googletest\googletest\include -DGTEST_MAIN_LIBRARY=%root_folder%\externals\build_gtest\Debug\gtest_maind.lib ../ -G "Visual Studio 15 2017 Win64"
 rem cmake.exe  -DZLIB_INCLUDE_DIR=D:\projects\zlib -DZLIB_LIBRARY=D:\projects\zlib\zlib.lib ../ -G "Visual Studio 15 2017 Win64"
 
 start vds.sln 
--- externals/build_gtest.bat
+++ externals/build_gtest.bat
@@ -6,5 +6,5 @@ rem git clone https://github.com/google/googletest.git
 mkdir %root_folder%\build_gtest
 cd %root_folder%\build_gtest
 
-cmake -G "Visual Studio 15 2017 Win64" %root_folder%\googletest\googletest
+cmake -G "Visual Studio 15 2017 Win64" -Dgtest_force_shared_crt=ON %root_folder%\googletest\googletest
 cmake --build .
