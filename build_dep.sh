60d6b779d5ce3a3301093faa822e91b6b5cc1eb4
UNI\v.malyshev <v.malyshev@innopolis.ru>
2018-12-26 11:30:48 +0300

    fix CI

--- build_dep.sh
+++ build_dep.sh
@@ -29,8 +29,8 @@ cd build
 cmake ..
 make --quiet cxx
 make --quiet install-cxx install-cxxabi
-export CXXFLAGS="-fcoroutines-ts -std=c++17 -fexceptions -stdlib=libc++ -fPIC -static"
-export LDFLAGS="-stdlib=libc++ -lc++ -lc++abi -lm -ldl -static"
+export CXXFLAGS="-fcoroutines-ts -std=c++17 -fexceptions -stdlib=libc++ -fPIC"
+export LDFLAGS="-stdlib=libc++ -lc++ -lc++abi -lm -ldl"
 cd ../..
 
 set +e
@@ -38,8 +38,8 @@ git clone --single-branch --branch OpenSSL_1_1_1-stable --quiet https://github.c
 rm -rf openssl-out
 set -e
 
-export CXXFLAGS="-fcoroutines-ts -std=c++17 -fexceptions -stdlib=libc++ -fPIC -static"
-export LDFLAGS="-stdlib=libc++ -lc++ -lc++abi -lm -ldl -static"
+export CXXFLAGS="-fcoroutines-ts -std=c++17 -fexceptions -stdlib=libc++ -fPIC"
+export LDFLAGS="-stdlib=libc++ -lc++ -lc++abi -lm -ldl"
 
 cd openssl
 ./Configure linux-armv4 no-tests no-asm --prefix=$PWD/../openssl-out --openssldir=$PWD/../openssl-out
