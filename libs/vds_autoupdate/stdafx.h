8eacf0a22765ec48001527dba4cf89227674aae6
Vadim Malyshev <lboss75@gmail.com>
2019-03-29 09:45:19 +0300

    --

---
+++ libs/vds_autoupdate/stdafx.h
@@ -0,0 +1,18 @@
+#ifndef __VDS_SERVER_STDAFX_H_
+#define __VDS_SERVER_STDAFX_H_
+
+/*
+Copyright (c) 2017, Vadim Malyshev, lboss75@gmail.com
+All rights reserved
+*/
+
+#include "vds_core.h"
+#include "vds_data.h"
+#include "vds_database.h"
+#include "vds_http.h"
+#include "vds_parser.h"
+#include "vds_crypto.h"
+
+#define ThisModule "AutoUpdate"
+
+#endif // __VDS_SERVER_STDAFX_H_
